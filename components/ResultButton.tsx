eeae87aa1103bc9ab4c583585906a564d2113224
GoktugYalcin <goktugyalcin06@gmail.com>
2024-01-07 00:36:40 +0300

    Fix

--- components/ResultButton.tsx
+++ components/ResultButton.tsx
@@ -12,9 +12,11 @@ const ResultButton: React.FC<{ data: Res }> = ({ data }) =>
       className="flex justify-center items-center text-bold bg-slate-100 rounded p-4 px-6 gap-4 text-2xl transition-opacity ease-in duration-700"
     >
       <input
-        value={`short.gokyalc.in/${(data as Res).hashed_url}`}
+        value={`short.gokyalc.in/h/${(data as Res).hashed_url}`}
         readOnly={true}
-        className={"bg-slate-100 border-r-2 pr-2 border-r-slate-300"}
+        className={
+          "bg-slate-100 border-r-2 pr-2 border-r-slate-300 text-slate-900"
+        }
       />
       <CopyIcon />
     </motion.span>
