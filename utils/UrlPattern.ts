dcdf91d389340cfe3b034c0746a999558663fc05
GoktugYalcin <goktugyalcin06@gmail.com>
2024-01-07 17:30:39 +0300

    fix: Removed debugger

--- utils/UrlPattern.ts
+++ utils/UrlPattern.ts
@@ -1,7 +1,5 @@
 const pattern: RegExp =
   /(https:\/\/www\.|http:\/\/www\.|https:\/\/|http:\/\/)?[a-zA-Z]{2,}(\.[a-zA-Z]{2,})(\.[a-zA-Z]{2,})?\/[a-zA-Z0-9]{2,}|((https:\/\/www\.|http:\/\/www\.|https:\/\/|http:\/\/)?[a-zA-Z]{2,}(\.[a-zA-Z]{2,})(\.[a-zA-Z]{2,})?)|(https:\/\/www\.|http:\/\/www\.|https:\/\/|http:\/\/)?[a-zA-Z0-9]{2,}\.[a-zA-Z0-9]{2,}\.[a-zA-Z0-9]{2,}(\.[a-zA-Z0-9]{2,})?/;
 
-export const checkPattern = (testUrl: string | undefined): boolean => {
-  debugger;
-  return !!testUrl ? pattern.test(testUrl) : false;
-};
+export const checkPattern = (testUrl: string | undefined): boolean =>
+  !!testUrl ? pattern.test(testUrl) : false;
